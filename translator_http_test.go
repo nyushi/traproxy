89618fd53bf71890f7e07d48bba1879ce9df0c9a
Yushi Nakai <nyushi@users.noreply.github.com>
2014-05-12 01:10:19 +0900

    improve coverage

--- translator_http_test.go
+++ translator_http_test.go
@@ -3,6 +3,7 @@ package traproxy
 import (
 	"net"
 	"testing"
+	"time"
 )
 
 func getHTTPTranslator(network, endpoint string) (client, proxy *net.TCPConn, trans *HTTPTranslator, err error) {
@@ -15,6 +16,7 @@ func getHTTPTranslator(network, endpoint string) (client, proxy *net.TCPConn, tr
 	if err != nil {
 		return nil, nil, nil, err
 	}
+
 	base := TranslatorBase{
 		Client: a.B,
 		Proxy:  b.B,
@@ -46,5 +48,32 @@ func TestHTTPTranslatorStartSuccess(t *testing.T) {
 	expected := "HEAD http://localhost/test HTTP/1.0\r\nHost: localhost\r\n\r\n"
 	if got != expected {
 		t.Errorf("got=%s\nexpected=%s", got, expected)
+	}
+}
+
+func TestHTTPTranslatorStartSuccessDoNothing(t *testing.T) {
+	client, proxy, trans, err := getHTTPTranslator("tcp", "127.0.0.1:12345")
+	if err != nil {
+		t.Fatal(err)
+	}
+	go trans.Start()
+
+	buf := make([]byte, 1024)
+	client.Write([]byte("test"))
+
+	proxy.SetDeadline(time.Now().Add(time.Millisecond))
+	if _, err = proxy.Read(buf); err == nil {
+		t.Fatal("not timeouted")
+	}
+}
+
+func TestHTTPTranslatorStartNotTCP(t *testing.T) {
+	_, _, trans, err := getHTTPTranslator("unix", "/tmp/traproxy_test")
+	if err != nil {
+		t.Error(err)
+	}
+	err = trans.Start()
+	if err.Error() != "client socket is not tcp" {
+		t.Error("socket check failed")
 	}
 }
