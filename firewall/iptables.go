b85b5f9d17dcfe8c19ef0352be105cdf1d364195
Yushi Nakai <nyushi@users.noreply.github.com>
2016-03-26 07:32:11 +0900

    Add -with-fw-nat/exclude-reserved-addrs flag

--- firewall/iptables.go
+++ firewall/iptables.go
@@ -79,6 +79,17 @@ func GetRedirectDockerRules(excludes []string) []IPTablesRule {
 	return rules
 }
 
+func GetRedirectNATRules(excludes []string) []IPTablesRule {
+	rules := []IPTablesRule{}
+	for _, addr := range excludes {
+		rules = append(rules, []string{preroutingChain, "-t", "nat", "-p", "tcp", "-j", accept, "-d", addr})
+	}
+
+	rules = append(rules, []string{preroutingChain, "-t", "nat", "-p", "tcp", "-j", redirect, "--dport", "80", "--to-ports", "10080"})
+	rules = append(rules, []string{preroutingChain, "-t", "nat", "-p", "tcp", "-j", redirect, "--dport", "443", "--to-ports", "10080"})
+	return rules
+}
+
 // LocalAddrs returns assigned local address
 func LocalAddrs() ([]string, error) {
 	addrs, err := net.InterfaceAddrs()
@@ -106,4 +117,26 @@ func GrepV4Addr(addrs []string) []string {
 		v4addrs = append(v4addrs, v)
 	}
 	return v4addrs
+}
+
+// ReservedV4Addrs returns reserved ipv4 addresses
+func ReservedV4Addrs() (addrs []string) {
+	return []string{
+		"0.0.0.0/8",
+		"10.0.0.0/8",
+		"100.64.0.0/10",
+		"127.0.0.0/8",
+		"169.254.0.0/16",
+		"172.16.0.0/12",
+		"192.0.0.0/24",
+		"192.0.2.0/24",
+		"192.88.99.0/24",
+		"192.168.0.0/16",
+		"198.18.0.0/15",
+		"198.51.100.0/24",
+		"203.0.113.0/24",
+		"224.0.0.0/4",
+		"240.0.0.0/4",
+		"255.255.255.255",
+	}
 }
