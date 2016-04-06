a19a916f17c1943d9bd3bd76282165c8974e69e8
Yushi Nakai <nyushi@users.noreply.github.com>
2016-04-07 05:36:04 +0900

    Add test

--- firewall/iptables_test.go
+++ firewall/iptables_test.go
@@ -64,3 +64,17 @@ func TestGetRedirectDockerRules(t *testing.T) {
 		t.Error(got, expected)
 	}
 }
+
+func TestGetRedirectNATRules(t *testing.T) {
+	rules := GetRedirectNATRules([]string{"127.0.0.1/8"})
+	got := ""
+	expected := "iptables PREROUTING -t nat -p tcp -j ACCEPT -d 127.0.0.1/8\n"
+	expected += "iptables PREROUTING -t nat -p tcp -j REDIRECT --dport 80 --to-ports 10080\n"
+	expected += "iptables PREROUTING -t nat -p tcp -j REDIRECT --dport 443 --to-ports 10080\n"
+	for _, r := range rules {
+		got += r.GetCommandStr() + "\n"
+	}
+	if got != expected {
+		t.Error(got, expected)
+	}
+}
